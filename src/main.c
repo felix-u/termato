d3d01645b63ba2c6f5db71f3aab1f998155d021a
felix-u <29074327+felix-u@users.noreply.github.com>
2023-02-22 22:48:13 -0500

    implement user-modifiable duration settings

--- src/main.c
+++ src/main.c
@@ -13,11 +13,47 @@
 
 
 #define TIME_STR_LEN 16
+#define DEFAULT_WORK (25 * 60)
+#define DEFAULT_BREAK_SHORT (5 * 60)
+#define DEFAULT_BREAK_LONG (20 * 60)
+#define DEFAULT_SESSIONS 4
 
 
 int main(int argc, char **argv) {
 
+    args_Flag work_flag = {
+        .name_short = 'w',
+        .name_long = "work",
+        .help_text = "specify the length of time for work periods, in minutes (default 25)",
+        .type = ARGS_SINGLE_OPT,
+        .expects = ARGS_EXPECTS_NUM,
+    };
+    args_Flag break_short_flag = {
+        .name_short = 'b',
+        .name_long = "short-break",
+        .help_text = "specify the length of time for short break periods, in minutes (default 5)",
+        .type = ARGS_SINGLE_OPT,
+        .expects = ARGS_EXPECTS_NUM,
+    };
+    args_Flag break_long_flag = {
+        .name_short = 'l',
+        .name_long = "long-break",
+        .help_text = "specify the length of time for long break periods, in minutes (default 20)",
+        .type = ARGS_SINGLE_OPT,
+        .expects = ARGS_EXPECTS_NUM,
+    };
+    args_Flag sessions_flag = {
+        .name_short = 's',
+        .name_long = "sessions",
+        .help_text = "specify the number of work periods before a long break (default 4)",
+        .type = ARGS_SINGLE_OPT,
+        .expects = ARGS_EXPECTS_NUM,
+    };
     args_Flag *flags[] = {
+        &work_flag,
+        &break_short_flag,
+        &break_long_flag,
+        &sessions_flag,
         &ARGS_HELP_FLAG,
         &ARGS_VERSION_FLAG,
     };
@@ -32,9 +68,15 @@ int main(int argc, char **argv) {
     });
     if (args_return != ARGS_RETURN_CONTINUE) return args_return;
 
-    usize duration = 5;
+    usize duration_work = work_flag.is_present ? atoi(work_flag.opts[0]) * 60 : DEFAULT_WORK;
+    usize duration_break_short =
+        break_short_flag.is_present ? atoi(break_short_flag.opts[0]) * 60 : DEFAULT_BREAK_SHORT;
+    usize duration_break_long =
+        break_long_flag.is_present ? atoi(break_long_flag.opts[0]) * 60 : DEFAULT_BREAK_LONG;
+    usize sessions = sessions_flag.is_present ? atoi(sessions_flag.opts[0]) * 60 : DEFAULT_SESSIONS;
+
     time_t start_time = time(NULL);
-    time_t end_time = start_time + duration;
+    time_t end_time = start_time + duration_work;
 
     int width = 0, height = 0;
     initscr();
@@ -44,15 +86,19 @@ int main(int argc, char **argv) {
     nodelay(stdscr, TRUE);
     for (;; napms(1), erase(), refresh()) {
         usize remaining_time = end_time - time(NULL);
+        usize remaining_mins = remaining_time / 60;
+        usize remaining_secs = remaining_time % 60;
 
         getmaxyx(stdscr, height, width);
 
         char remaining_time_str[TIME_STR_LEN];
-        snprintf(remaining_time_str, TIME_STR_LEN, "%ld", remaining_time);
+        snprintf(remaining_time_str, TIME_STR_LEN, "%02ld:%02ld", remaining_mins, remaining_secs);
         mvaddstr(height / 2, (width - strlen(remaining_time_str)) / 2, remaining_time_str);
 
+        timeout(1000);
+
         int c = getch();
-        if (c == 'q' || remaining_time == 0) break;
+        if (c == 'q') break;
     }
     endwin();
 
